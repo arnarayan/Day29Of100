63a3e6eb483aafe74bab124256b80dd109d4b575
Anand.Narayan <arnarayan@gmail.com>
2022-12-26 19:27:50 -0700

    Initial Commit

---
+++ Day29Of100/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Day29Of100
+//
+//  Created by Anand Narayan on 2022-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day29Of100/Day29Of100App.swift
@@ -0,0 +1,17 @@
+//
+//  Day29Of100App.swift
+//  Day29Of100
+//
+//  Created by Anand Narayan on 2022-12-26.
+//
+
+import SwiftUI
+
+@main
+struct Day29Of100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
